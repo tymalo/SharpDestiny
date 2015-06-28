9ada0a2819de768f7fbf8fd78ba0a0cf50e4e49b
Vasile Laur <laurvasile@yahoo.com>
2015-06-28 18:22:30 -0400

    Adding Temporary Web Proj

---
+++ SharpDestiny.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharpDestiny.Web.Startup))]
+namespace SharpDestiny.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
