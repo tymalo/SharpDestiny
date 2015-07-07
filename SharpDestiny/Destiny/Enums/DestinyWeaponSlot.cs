7c4fedb0d048524362a2ab94dffb0b79eb1f4440
Vasile Laur <laurvasile@yahoo.com>
2015-07-06 21:46:39 -0400

    some small refactoring

---
+++ SharpDestiny/Destiny/Enums/DestinyWeaponSlot.cs
@@ -0,0 +1,16 @@
+﻿using SharpDestiny.Common;
+
+namespace SharpDestiny.Destiny.Enums
+{
+    public enum DestinyWeaponSlot
+    {
+        [DisplayText("Primary")]
+        Primary = 1,
+        [DisplayText("Special")]
+        Special = 2,
+        [DisplayText("Heavy")]
+        Heavy = 3,
+        [DisplayText("Relic")]
+        Relics = 4,
+    }
+}
